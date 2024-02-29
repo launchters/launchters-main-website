1e2b73f588ff53c108cd72613ca8e6c1cbc28378
Alex Launchters <launchters@gmail.com>
2024-02-29 06:50:08 +0200

    Encapsulating Routes inside AppRouter...

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 // import privacyDoc from "./config/legal/privacy.md";
 // import termsOfServiceDoc from "./config/legal/termsOfService.md";
 // import LegalDoc from "./modules/views/pages/legal/LegalDoc";
 import theme from "./config/theme";
-import HomeIndex from "./modules/views/pages/HomeIndex";
-import GeneralTermSheet from "./modules/views/pages/legal/contracts/GeneralTermSheet";
+import AppRouter from "./routes/AppRouter";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Routes>
-          {/* <Route
-            path="/legal/terms"
-            element={<LegalDoc doc={termsOfServiceDoc} />}
-          />
-          <Route
-            path="/legal/privacy"
-            element={<LegalDoc doc={privacyDoc} />}
-          /> */}
-          <Route
-            path="/legal/term-sheets/general"
-            element={<GeneralTermSheet />}
-          />
-          <Route path="/" element={<HomeIndex />} />
-        </Routes>
-      </Router>
+      <AppRouter />
     </ThemeProvider>
   );
 }
---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,51 @@
+import { Route, Routes } from "react-router-dom";
+import "../App.css";
+// import privacyDoc from "../config/legal/privacy.md";
+// import termsOfServiceDoc from "../config/legal/termsOfService.md";
+import HomeIndex from "../modules/views/pages/HomeIndex";
+import LegalDoc from "../modules/views/pages/legal/LegalDoc";
+
+const dummyDoc = "# Heck Yes\n\nThis is great!";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      {/* <Route
+        path="/legal/terms"
+        element={
+          <LegalDoc title="Términos del Servicio" doc={termsOfServiceDoc} />
+        }
+      />
+      <Route
+        path="/legal/privacy"
+        element={
+          <LegalDoc
+            title="Política de Privacidad y Uso de Cookies"
+            doc={privacyDoc}
+          />
+        }
+      /> */}
+      {/* <Route
+        path="/legal/term-sheets/general"
+        element={
+          <LegalDoc
+            title="Condiciones Generales (CCGG)"
+            doc={contractGeneralTermsDoc}
+          />
+        }
+      /> */}
+
+      <Route
+        path="/legal/privacy"
+        element={
+          <LegalDoc
+            title="Política de Privacidad y Uso de Cookies"
+            doc={dummyDoc}
+          />
+        }
+      />
+
+      <Route path="/" element={<HomeIndex />} />
+    </Routes>
+  );
+}
