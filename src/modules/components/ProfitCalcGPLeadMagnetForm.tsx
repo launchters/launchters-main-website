2d2eec3b7f76ad800a4d7db30bc84407a9623fa0
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-02-29 12:32:04 +0100

    Create ProfitCalcGPLeadMagnetForm.tsx
    Nuevo archivo ProfitClac... creado y añadido la estructura.

---
+++ src/modules/components/ProfitCalcGPLeadMagnetForm.tsx
@@ -0,0 +1,9 @@
+import Typography from "./Typography";
+
+const ProfitCalcGPLeadMagnetForm = () => { 
+    return (
+        <Typography>AQUÍ IRÁ MI FORMULARIO PARA ESTE CASO DE USO (USE CASE)</Typography>
+    )
+};
+
+export default ProfitCalcGPLeadMagnetForm;
