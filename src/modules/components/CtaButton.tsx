3502cc43503df80432510f7077460343d07cfe92
Alex Launchters <launchters@gmail.com>
2024-03-04 16:52:38 +0200

    Adding button to see Lead Magnet's form

--- src/modules/components/CtaButton.tsx
+++ src/modules/components/CtaButton.tsx
@@ -40,6 +40,26 @@ export default function CtaButton() {
       >
         con Alex Launchters como mi Socio de Crecimiento (Growth Partner)
       </Typography>
+
+      <br />
+      <br />
+      <Button
+        color="primary"
+        variant="outlined"
+        size="small"
+        component="a"
+        href={"/#/lm/profit-calc-gp-001"}
+      >
+        <Stack flexDirection="row" spacing={2}>
+          <Typography
+            variant="h6"
+            component="span"
+            sx={{ fontSize: { xs: "1.2rem", md: "2em" } }}
+          >
+            Ver formulario de David
+          </Typography>
+        </Stack>
+      </Button>
     </Stack>
   );
 }
