f4f63243bb3bc3dd3ae410a8ff45fbf3972221f8
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-03-11 11:14:11 +0100

    Leve corrección de dirección de un import
    Corregido el enlace del import FormStepper

--- src/modules/components/MyForm/ProfitCalcGPLeadMagnetForm.tsx
+++ src/modules/components/MyForm/ProfitCalcGPLeadMagnetForm.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { $TSFix } from "../../models/ts-fix.d";
 import { IFormInput } from "./models/FormInputTypes";
-import { FormStepper } from "./FormStepper";
+import { FormStepper } from "./partials/FormStepper";
 import { EmailStep } from "./Steps/EmailStep";
 import { InstagramStep } from "./Steps/InstagramStep";
 import { InstagramViewsStep } from "./Steps/InstagramViewsStep";
