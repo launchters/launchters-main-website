932f501bdfef1ba2c19c9879f83a997ebd22342f
Alex Launchters <launchters@gmail.com>
2024-03-14 12:27:37 +0200

    Implementing a down sized Offer for the non qualifying prospects

--- src/modules/components/MyForm/partials/DownSizedOffer.tsx
+++ src/modules/components/MyForm/partials/DownSizedOffer.tsx
@@ -1,12 +1,16 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import { Suspense, useState } from "react";
 import appConfig from "../../../../config/app.config";
 import ButtonCustom from "../../ButtonCustom";
+import { FaSadTear } from "react-icons/fa";
 
 const vslStorageUrl = `${appConfig.storageUrls.vslVideos}/Growth-Partner-Program/down-size-offer`;
-const vslVideoVariants: { [key: string]: string[] } = {
+const vslVideoVariants = {
   gppWorkshopWaitlist: [
-    `${vslStorageUrl}/workshop/1.%20Waitlist/VSL-workshop-waitlist-1.mp4?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEGkaDGV1LWNlbnRyYWwtMSJHMEUCIAxEAqpph3pn5iYJmM9Xl9DAfEYjvBcnADiVSlJhx2q%2BAiEAu3l69Uq0RMGEU4mWi056zBfE4snsd4TJyGQS9%2FoAijsq5AIIchAAGgwzODE0OTIxMDY2NTAiDLj3jS0Kha%2FLFa639irBAv56fInV%2FheQFRGsU5zBPhXH5hu8PlakoDhx8CXYq8z8%2BOvOzM6UHTI%2FRtvuE85fNZDxNmGSQimHiww9UhEq4PB0ZkCaBW6RyrngphqsnBsqbY%2FzFyhCzwgCWtTyg9QQCti752Wbd8G17wvOCopWKxD1aJScHsrPkJXv9WlHVSe0GHTM2kKvfSw%2F3emvE658WO%2BJ%2BhE0aHXt6J%2Fv8uA2TtUEEr82l5yp73D2Wl4DMn3NQE02bClfHRWp66KGBWcwPCA16yakWK1MvihUzYShxHEuos1UJMznp0G0bMgzr6yreLuLEYUKq7EhQJDtt%2B4BMXDd%2BjZaOT5fq5rtt12z4tJfRQnXbnT0hpsufD5ehJrxOHVlXBbS5j8C8SCpqMX7P0NZz%2FdNvx8PeJRk3P4ACYF0lfmnlUrvJPyxyremnOWaMzCq7sqvBjqzAtIdDOKShZRi0yHDEMByzq9F%2FG5h0A7Z26eqf6VEzWJHxCcZMHbr8tDzFDPKRuOdETAgn6CQxlUPUIDf5OWg3iv6ju1dsEdhf%2FnqNELdEJ54FU6H7eoplecqz5UCS1VUt4wXFzH1kHD%2BpMFFShY1jHacqRcsjMyweTwCZyADhJ%2FBKE8nm7I3CeSkoOAXZo84uxcbt6H90g8BGMXs4sc%2B%2Fo3DFpbQ%2BhnBliKHbSAxMf2npzm7cfFviacGrO0wYX5%2BKHNDwOl3CBI0nB3MkWPkgIiVbWCVePFbkVPDO7vGHfz2e32v7VnzqTgy8yJZOaH%2FxNTA4%2BFNM%2BYMzzpijlTocEujEUaxizk8Abz7M%2FQqQ7quP2JPblLyAYUG8euQ0zPeUimUGwqIWgZWxp2m0i6PdXoOqU0%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240314T094518Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAVRUVTSWNJM5UVDFE%2F20240314%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Signature=fd7fbb68c97204c72e4d969f62cc452873329ea3f02bebba3638808c59ca317e`,
+    {
+      src: `${vslStorageUrl}/workshop/1.+Waitlist/VSL-workshop-waitlist-1.mp4`,
+      cover: `${vslStorageUrl}/workshop/1.+Waitlist/VSL-workshop-waitlist-1-Cover.jpg`,
+    },
   ],
 };
 
@@ -40,36 +44,47 @@ function GrowthPartnerWorkshopWaitlist() {
   };
 
   return (
-    <>
-      <Box sx={{ maxWidth: "42rem", margin: "0 auto" }}>
-        <Typography variant="h3">
-          pero VAS A PODER CRECER con este regalazo
-        </Typography>
-        <Stack sx={{ maxWidth: "61.8vh", margin: "1rem auto" }}>
-          <Suspense fallback={`El vídeo está cargando...`}>
-            <video controls autoPlay>
-              <source src={selectedVariant} type="video/mp4" />
-              Tu navegador no soporta el vídeo.
-            </video>
-          </Suspense>
-        </Stack>
-        <Typography sx={{ my: 2 }} variant="h6">
-          En este evento online, te ayudaré a pasar de 0€/mes a vivir de tu
-          pasión como coach y llegar a facturar +3.000€/mes.
-        </Typography>
+    <Box sx={{ width: "min(50rem,90%)", margin: "0 auto" }}>
+      <FaSadTear size={50} />
+      <Typography variant="h6">
+        Tu perfil no reune los requisitos para el programa <i>Growth Partner</i>{" "}
+        <u>todavía</u>,
+      </Typography>
 
-        <Typography sx={{ my: 2 }}>
-          trabajarás en grupo para dar los pasos necesarios para iniciar tu
-          negocio de coaching con éxito y llevarlo a un punto donde puedas vivir
-          de él. No solo te aportaré las claves necesarias sino que te guiaré y
-          trabajaremos juntos en esos puntos, evitando los errores típicos y
-          resolviendo los problemas iniciales. Se acabó el vivir una vida que no
-          quieres.
-        </Typography>
-        <ButtonCustom variant="contained" onClick={handleConfirmation}>
-          ¡Yo quiero, apúntame a esa lista ya!
-        </ButtonCustom>
-      </Box>
-    </>
+      <Typography sx={{ my: 2 }} variant="h3">
+        ⬇️Pero vas a poder crecer con este regalazo 🎁
+      </Typography>
+      <Stack sx={{ margin: "1rem auto" }}>
+        <Suspense fallback={`Cargando vídeo...`}>
+          <video
+            controls
+            autoPlay={false}
+            poster={selectedVariant.cover}
+            width={500}
+            style={{ margin: "0 auto" }}
+          >
+            <source src={selectedVariant.src} type="video/mp4" />
+            Tu navegador no soporta el vídeo.
+          </video>
+        </Suspense>
+      </Stack>
+
+      <Typography sx={{ my: 2 }} variant="h6">
+        te ayudaré a pasar de 0€/mes a vivir de tu pasión como coach y llegar a
+        facturar +3.000€/mes.
+      </Typography>
+
+      <Typography sx={{ my: 2 }}>
+        trabajarás en grupo para dar los pasos necesarios para iniciar tu
+        negocio de coaching con éxito y llevarlo a un punto donde puedas vivir
+        de él. No solo te aportaré las claves necesarias sino que te guiaré y
+        trabajaremos juntos en esos puntos, evitando los errores típicos y
+        resolviendo los problemas iniciales. Se acabó el vivir una vida que no
+        quieres.
+      </Typography>
+      <ButtonCustom variant="contained" onClick={handleConfirmation}>
+        ¡Yo quiero, apúntame a esa lista ya!
+      </ButtonCustom>
+    </Box>
   );
 }
--- src/modules/components/MyForm/partials/NoQualifiedResult.tsx
+++ src/modules/components/MyForm/partials/NoQualifiedResult.tsx
@@ -1,20 +1,7 @@
-import { Container, Typography } from "@mui/material";
-import React from "react";
-import { FaSadTear } from "react-icons/fa";
 import DownSizedOffer from "./DownSizedOffer";
 
-const NoQualifiedResult: React.FC = () => {
-  return (
-    <Container>
-      <FaSadTear size={50} />
-      <Typography variant="h6">
-        Tu perfil no reune los requisitos para el programa <i>Growth Partner</i>{" "}
-        <u>todavía</u>,
-      </Typography>
-
-      <DownSizedOffer variant="gpp-workshop-waitlist" />
-    </Container>
-  );
+const NoQualifiedResult = () => {
+  return <DownSizedOffer variant="gpp-workshop-waitlist" />;
 };
 
 export default NoQualifiedResult;
